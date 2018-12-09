ec40fc94b2b5db510c6cc2963b614cdc5a13df4f
Cristopher Palacios <criszelaya24@hotmail.com>
2018-12-09 14:08:52 +0000

    Starting frame in zero and updating it after score

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -5,12 +5,17 @@ describe('Game', function(){
 	beforeEach(function(){
 		bowling = new Bowling;
 	});
+	it('Start in fram equa a 0',function(){
+		expect(bowling.getCurrentFrame()).toEqual(0);
+	});
 	it('Start with total of 0', function(){
 		expect(bowling.totalScore()).toEqual(0);
 	});
 
 	it('Score the first frame!', function(){
 		bowling.score(1,3);
-		expect(bowling.totalScore()).toEqual(4);
+		bowling.score(4,2);
+		expect(bowling.totalScore()).toEqual(10);
+		expect(bowling.getCurrentFrame()).toEqual(2);
 	});
 });
--- src/bowling.js
+++ src/bowling.js
@@ -3,18 +3,33 @@
 function Bowling() {
 	this.points = [];
 	this.total = 0;
+	this.totalRolls = 10;
+	this.bonusPerframe = [];
+	this.currentFrame = 0;
 }
+
+Bowling.prototype.getCurrentFrame = function() {
+	return this.currentFrame
+};
 
 Bowling.prototype.totalScore = function() {
 	for (var i = 0; i < this.points.length; i++) {
-		for (var j = 0; j < i; j++) {
-			this.total += this.points[j];
-			this.total += this.points[i];
+		for (var j = 0; j < this.points.length; j++) {
+			this.total += this.points[i][j];
 		}
 	}
 	return this.total;
 };
 
-Bowling.prototype.score = function(value_1, value_2 = 0) {
-	this.points.push(1, 3);
+Bowling.prototype.score = function(shot_1 , shot_2 ) {
+	this.currentFrame += 1;
+	if (shot_1 === this.totalRolls) {
+		this.points.push([shot_1, 0]);
+		// this.bonus();
+	}
+	if (shot_2 === this.totalRolls) {
+		this.points.push([0, shot_2]);
+		// this.bonus();
+	}
+	this.points.push([shot_1, shot_2]);
 };
