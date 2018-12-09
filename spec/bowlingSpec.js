957808a7db1bc032e08892dd1adc356fba527623
Cristopher Palacios <criszelaya24@hotmail.com>
2018-12-09 14:59:16 +0000

    Getting bonus

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -11,11 +11,26 @@ describe('Game', function(){
 	it('Start with total of 0', function(){
 		expect(bowling.totalScore()).toEqual(0);
 	});
-
+	it('Validating an score more than 10 rolls', function(){
+		expect(bowling.score(10,3)).toEqual('Invalid rolls!');
+	});
 	it('Score the first frame!', function(){
 		bowling.score(1,3);
 		bowling.score(4,2);
 		expect(bowling.totalScore()).toEqual(10);
 		expect(bowling.getCurrentFrame()).toEqual(2);
+	});
+
+	it('Validating an score of X', function(){
+		bowling.score(0,10);
+		bowling.score(5,5);
+		expect(bowling.totalScore()).toEqual(20);
+	});
+
+	it('Score an spare an returning bonus!', function(){
+		bowling.score(5,5);
+		bowling.score(10,0);
+		bowling.score(1,2);
+		expect(bowling.bonus()).toEqual(11)
 	});
 });
--- src/bowling.js
+++ src/bowling.js
@@ -5,7 +5,9 @@ function Bowling() {
 	this.total = 0;
 	this.totalRolls = 10;
 	this.bonusPerframe = [];
+	this.bono = 0;
 	this.currentFrame = 0;
+	this.validationScore = 0;
 }
 
 Bowling.prototype.getCurrentFrame = function() {
@@ -16,20 +18,40 @@ Bowling.prototype.totalScore = function() {
 	for (var i = 0; i < this.points.length; i++) {
 		for (var j = 0; j < this.points.length; j++) {
 			this.total += this.points[i][j];
+
 		}
 	}
 	return this.total;
 };
 
-Bowling.prototype.score = function(shot_1 , shot_2 ) {
+Bowling.prototype.score = function(shot_1 = 0 , shot_2 = 0 ) {
 	this.currentFrame += 1;
+	this.validationScore = shot_1 + shot_2;
+
+	if (this.validationScore > this.totalRolls) {
+		return 'Invalid rolls!';
+	}
 	if (shot_1 === this.totalRolls) {
 		this.points.push([shot_1, 0]);
-		// this.bonus();
 	}
 	if (shot_2 === this.totalRolls) {
 		this.points.push([0, shot_2]);
-		// this.bonus();
+	}else {
+		this.points.push([shot_1, shot_2]);
 	}
-	this.points.push([shot_1, shot_2]);
 };
+
+Bowling.prototype.bonus = function() {
+	for (var i = 0; i < this.points.length; i++) {
+		for (var j = 0; j < this.points.length; j++) {
+			this.total += this.points[i][j];
+			if (this.points[i][j] === this.totalRolls) {
+				this.bono += this.points[i+1][j];
+				this.bonusPerframe.push([this.currentFrame, this.points[i][j+1]]);
+			}
+		}
+	}
+	return this.bono;
+};
+
+// [[5,5], [10,0], [1,2]]
